060fafe739d2fb709f6312a4c0ae62cc52823e3a
Giordano <giordano.mendes94@gmail.com>
2023-03-01 20:55:30 -0300

    feat: create Team model

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,25 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+
+export default class TeamModel extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+TeamModel.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+  modelName: 'teams',
+});
