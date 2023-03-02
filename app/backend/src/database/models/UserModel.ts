280194676ab7efa684f9f72282968d3230795953
Giordano <giordano.mendes94@gmail.com>
2023-03-02 11:27:21 -0300

    feat: create User model

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,42 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+
+export default class UserModel extends Model {
+  declare id: number;
+  declare userName: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+UserModel.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  userName: {
+    allowNull: false,
+    type: STRING,
+
+  },
+  role: {
+    allowNull: false,
+    type: STRING,
+  },
+  email: {
+    allowNull: false,
+    type: STRING,
+  },
+  password: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+  modelName: 'users',
+});
