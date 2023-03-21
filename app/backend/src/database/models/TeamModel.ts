4da7ed77ef5a4ecf73d5bf981d41191132841b35
Giordano <giordano.mendes94@gmail.com>
2023-03-21 13:59:09 -0300

    fix: error in TeamsModel

--- app/backend/src/database/models/TeamModel.ts
+++ app/backend/src/database/models/TeamModel.ts
@@ -2,12 +2,6 @@ import { INTEGER, Model, STRING } from 'sequelize';
 import db from '.';
 
 export default class TeamModel extends Model {
-  awayMatches(teamName: string, awayMatches: any, arg2: string[]): import("../../Interfaces/ILeaderBoard").default {
-    throw new Error('Method not implemented.');
-  }
-  homeMatches(teamName: string, homeMatches: any, arg2: string[]): import("../../Interfaces/ILeaderBoard").default {
-    throw new Error('Method not implemented.');
-  }
   declare readonly id: number;
   declare teamName: string;
 }
